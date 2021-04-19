bf2285cb6605b883b65a90dc39e63aa850145232
iktaecheon <78501309+iktaecheon@users.noreply.github.com>
2021-04-19 00:56:33 -0700

    completed

--- task1.py
+++ task1.py
@@ -6,3 +6,76 @@ the completed madlib.
 What is a madlib? Visit https://www.madlibs.com/printables/ to see some Madlibs
 you might use in your assignment
 """
+
+import tkinter as tk 
+from tkinter import *
+
+win = tk.Tk()
+win.title("Madlib")
+win.geometry("800x600")
+
+def are():
+    a = "are"
+    e2.insert(0,a)
+
+def arenot():
+    a = "aren't"
+    e2.insert(0,a)
+
+def PS4():
+    a = "PS4"
+    e1.insert(0,a)
+
+def XBOX():
+    a = "XBOX"
+    e1.insert(0,a)
+
+def Pokemon():
+    a = "Pokemon"
+    e3.insert(0,a)
+
+def Mario():
+    a = "Mario"
+    e3.insert(0,a)
+
+
+Labeldata = StringVar()
+
+label1 = Label(win, width=5, text="My ")
+e1 = Entry(win, textvariable=Labeldata, width=10)
+
+Button0 = Button(win, text="PS4", command=PS4)
+Button1 = Button(win, text="XBOX", command=XBOX)
+Button2 = Button(win, text="are", command=are)
+Button3 = Button(win, text="aren't", command=arenot)
+Button4 = Button(win, text="Pokemon", command=Pokemon)
+Button5 = Button(win, text="Mario", command=Mario)
+
+e2 = Entry(win, text="are/aren't", width=10)
+label2 = Label(win, width=15, text="perfect to play")
+e3 = Entry(win, text="adj", width=10)
+label3 = Label(win, width=15, text="!")
+
+blank1 = Label(win, width=15, text="")
+
+
+Button0.place(x=110, y=22)
+Button1.place(x=165, y=22)
+Button2.place(x=110, y=57)
+Button3.place(x=140, y=57)
+Button4.place(x=250, y=57)
+Button5.place(x=320, y=57)
+
+blank1.grid(row=2, column=1)
+
+label1.grid(row=1, column=1)
+label2.grid(row=3, column=3)
+label3.grid(row=3, column=5)
+
+e1.grid(row=1, column=2)
+e2.grid(row=3, column=2)
+e3.grid(row=3,column=4)
+
+Labeldata.set = "Noun"
+
+win.mainloop()
--- task2.py
+++ task2.py
@@ -12,3 +12,74 @@ The program should display the factored form in an Entry widget.
 Extension: make the + between a,b and b,c buttons that will toggle
 between + and -.
 """
+
+import tkinter as tk 
+from tkinter import *
+
+
+
+win = tk.Tk()
+win.title("Factorization Machine")
+
+win.geometry("800x400")
+
+
+def factorize():
+    finalTry.insert(0,"                    ")
+    b = int(Entry1.get())
+    c = int(Entry2.get())
+    
+    nums = []
+    for i in range(1, c + 1):
+        if c % i == 0:
+            nums.append(i)
+    nums.sort()
+    print(nums)
+    count = 0
+    xval = ""
+    yval = ""
+    q = 0
+    w = -1
+    while (nums[q]+nums[w] != b):
+        count = count+1
+        w = w-1
+        q=q+1
+       
+        xval = nums[q]
+        yval = nums[w]
+
+        xval = str(xval)
+        yval = str(yval)
+
+    finalTry.insert(0,("(x+", nums[q], ")(x+", nums[w], ")"))
+    print(xval + "  " + yval)
+
+
+
+
+label1 = Label(win, width=2, text="x^2")
+label2 = Label(win, width=2, text="x")
+label3 = Label(win, text="+", width=3)
+label4 = Label(win, text="+", width=3)
+Button1 = Button(win, text="factorize", command=factorize)
+
+
+Entry1 = Entry(win, text="b", width=3)
+Entry2 = Entry(win, text="c", width=3)
+finalTry = Entry(win, text="adj", width=15)
+
+
+
+label1.grid(row=1, column=1)
+label2.grid(row=1, column=4)
+label3.grid(row=1, column=2)
+label4.grid(row=1, column=5)
+
+Button1.place(x=0, y=100)
+
+
+Entry1.grid(row=1, column=3)
+Entry2.grid(row=1, column=6)
+finalTry.place(x=150, y=100)
+
+win.mainloop()
